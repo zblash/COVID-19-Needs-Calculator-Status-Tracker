25610b734ebe537453a178cc4d4bb74c304c532b
Yusuf Can Celik <yusufcancelik@hotmail.com>
2020-03-23 02:19:20 +0300

    test pages and context added

--- src/pages/corona-test/index.tsx
+++ src/pages/corona-test/index.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import { useCoronaTestContext } from "../../context";
 import { PageOneComponent } from "./page-one";
 
-export interface ICoronaTestPageProps {}
-
-function CoronaTestPage(props: React.PropsWithChildren<ICoronaTestPageProps>) {
+function CoronaTestPage() {
   const { coronaTestState, setCoronaTestState } = useCoronaTestContext();
 
   const handleNextOnClick = React.useCallback(() => {
